cf0c26b2ce3f56df36e84e427216ce9dc9128649
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-04 03:10:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,11 +47,15 @@ function removeFromCart(item) {
          for ( let i = 0; i < cart.length; i++){
 
           if (cart[i]['itemName'] === item)
+          {
           removed.push(cart.splice(i,1))
           return cart
+          }
           if (removed.length === 0)
-          return "the item is not in your cart"
-   }
+          {
+          return "That item is not in your cart"
+          }
+   }     
 }
 
 function placeOrder(cardNumber) {
