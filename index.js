fb46764727347ace69400d42f7aa3e1bca5cbef5
Min-Hsuan Hung <poxuan33@gmail.com>
2018-09-04 02:41:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
          for (let i = 0; i < cart.length; i++){
          if (i === cart.length - 1 && i != 0 )
          {
-         viewItems.push(`and ${cart[i]['itemName']} at $${cart['[i]itemPrice']}`)
+         viewItems.push(`and ${cart[i]['itemName']} at $${cart[i]['itemPrice']}`)
          }
          else{
               viewItems.push(`${cart[i][itemName]} at $${cart[i]['itemPrice']}`)
